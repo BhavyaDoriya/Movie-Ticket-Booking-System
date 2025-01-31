c47e77d38f2db3b2e96f49dcfc7ced6d9acaf973
Bhavya <bhavyadoriya2007@gmail.com>
2025-01-31 21:18:53 +0530

    Added Classes

--- MovieTicketBooking.java
+++ MovieTicketBooking.java
@@ -1,4 +1,39 @@
-class Movie
+class Movies
+{
+    public static void main(String...args)
+    {
+
+    }
+}
+class ComedyMovies extends Movies
+{
+
+}
+class ActionMovies extends Movies
+{
+
+}
+class SliceOfLifeMovies extends Movies
+{
+
+}
+class HorrorMovies extends Movies
 {
     
+}
+class ScienceFictionMovies extends Movies
+{
+
+}
+class RomanceMovies extends Movies
+{
+
+}
+class AnimatedMovies extends Movies
+{
+
+}
+class DocumentryMovies extends Movies
+{
+
 }
